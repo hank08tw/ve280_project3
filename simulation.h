408d335164ba32d827217140a52f71fe0522d05f
hank08tw <hank08tw@sjtu.edu.cn>
2018-06-30 22:20:38 +0800

    .

--- simulation.h
+++ simulation.h
@@ -2,5 +2,6 @@
 
 #ifndef SIMULATION_H
 #define SIMULATION_H
-
+int read_summaryfile_and_speciesfiles(world_t& theworld,const string& summary_file_name);
+int read_world_file(world_t& theworld,const string& world_file_name);
 #endif//SIMULATION_H
